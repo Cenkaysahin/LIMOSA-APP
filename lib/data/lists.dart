bf470d5f0280241337d1e78ec556bc2cadf41e5d
tekin <mkubilaytekin@gmail.com>
2024-03-18 12:27:39 +0300

    datalar eklendi

--- lib/data/lists.dart
+++ lib/data/lists.dart
@@ -3,20 +3,33 @@ import 'package:appjam/data/event_data.dart';
 class Lists{
   List<EventData> museum = [
     EventData('Galata Kulesi', 'images/galata.jpg'),
-    EventData('Ayasofya Cami', 'images/ayasofya.png')
+    EventData('Ayasofya Cami', 'images/ayasofya.png'),
+    EventData('Yerebatan Sarnacı ', 'images/yerebatansarnaci.png'),
+
   ];
   
   List<EventData> cafeRestaurant = [
     EventData('Nite Restoran', 'images/nite.png'),
-    EventData('Tugra Restoran', 'images/tugra.png')
+    EventData('Tugra Restoran', 'images/tugra.png'),
+    EventData('Balatte', 'images/balatte.png'),
+    EventData('Pancake House', 'images/pancakehouse.png'),
   ];
 
   List<EventData> transport = [
-    EventData('Vapur', 'images/ferry.png')
+    EventData('Vapur', 'images/ferry.png'),
+    EventData('Tren', 'images/train.png'),
+    EventData('Otobüs', 'images/bus.png'),
+    EventData('Taksi', 'images/taxi.png'),
+    EventData('Tramvay', 'images/tramway.png'),
+
   ];
   
   List<EventData> theater = [
-    EventData('Tiyatro', 'images/theater1.jpg')
+    EventData('Tiyatro', 'images/theater1.jpg'),
+    EventData('Sinema', 'images/cinema.png'),
+    EventData('Konser', 'images/concert.png'),
+    EventData('Opera', 'images/opera.png'),
+
   ];
   
 }
